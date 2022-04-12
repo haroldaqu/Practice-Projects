e679ef39da7ea74d0f7f481be45972d5aeccd156
haroldaqu <haroldgaquino@gmail.com>
2022-04-12 11:54:57 -0700

    Update main.js
    1. added score counter
    2.  added toLowerCase() to prompt of user
         - so that user can type in any cap
         case
    3. lower cased rock paper scissors array
    4.  added else if state when tie
          - so I can add else statement
         - before typing anything makes
           function equal to tie
    5. changed else to alert
         - this tells user to type in RNP
         - once they do playRound() will run
            again

--- RNP Project/main.js
+++ RNP Project/main.js
@@ -1,40 +1,50 @@
 // Random RPS generator
 function computerPlay() {
-    const rnp = ['Rock', 'Paper', 'Scissors']
+    const rnp = ['rock', 'paper', 'scissors']
     return rnp[Math.floor(Math.random()*3)]
 }
 
 
 // Make a function that will play a round of rock paper scissors
+let playerScore = 0;
+let computerScore = 0;
+
+
 
 function playRound() {
-    let playerSelection = prompt('Please enter selection')
+    let playerSelection = prompt('Please enter selection').toLowerCase();
     let computerSelection = computerPlay();
-    
-    if (((playerSelection == 'Scissors') && (computerSelection == 'Paper'))||
-        ((playerSelection == 'Rock') && (computerSelection == 'Scissors'))||
-        ((playerSelection == 'Paper') && (computerSelection == 'Rock'))) {
+    if (((playerSelection == 'scissors') && (computerSelection == 'paper'))||
+        ((playerSelection == 'rock') && (computerSelection == 'scissors'))||
+        ((playerSelection == 'paper') && (computerSelection == 'rock'))) {
         outcome = 'win'
         winner = playerSelection
         loser = computerSelection
         verbAct = 'beats'
-        whoWon = 'Player'   
+        whoWon = 'Player'
+        playerScore = playerScore +1;
+
     }
-    else if (((playerSelection == 'Scissors') && (computerSelection == 'Rock'))||
-             ((playerSelection == 'Rock') && (computerSelection == 'Paper'))||
-             ((playerSelection == 'Paper') && (computerSelection == 'Scissors'))) {
+    else if (((playerSelection == 'scissors') && (computerSelection == 'rock'))||
+             ((playerSelection == 'rock') && (computerSelection == 'paper'))||
+             ((playerSelection == 'paper') && (computerSelection == 'scissors'))) {
         outcome = 'lose'
         loser = playerSelection
         winner = computerSelection
         verbAct = 'beats'
         whowon = 'Computer'
+        computerScore = computerScore + 1;
     }
-    else {
+    else if (playerSelection === computerSelection) {
         outcome = 'tied';
         winner = playerSelection;
         loser = computerSelection;
         verbAct = 'ties with';
         whoWon = 'tied'
+    }
+    else {
+        alert('Type in Rock, Paper, or Scissors')
+        playRound();
     }
     return `You ${outcome}! ${winner} ${verbAct} ${loser}`
 }
@@ -46,6 +56,18 @@ function game() {
     for (let i = 1; i <= 5; i++) { 
         console.log(playRound()); 
     }
+    if (playerScore > computerScore) {
+        console.log('You win the game!')
+    }
+    else if (computerScore > playerScore) {
+        console.log('You lose the game!')
+    }
+    else {
+        console.log('Tie game!')
+    }
+    console.log('-------Score Below -------')
+    console.log(`Your Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
 }
 game();
 
